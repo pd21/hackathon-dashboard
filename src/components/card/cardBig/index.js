cfda9077365f57ad09cdb83a4b738a7af591a75e
Saravanan <saravananrohit1997@gmail.com>
2021-09-20 19:37:57 +0530

    added drag and drop

--- src/components/card/cardBig/index.js
+++ src/components/card/cardBig/index.js
@@ -1,15 +1,25 @@
-import React, { Component } from 'react'
-import './style.css'
+import React, { Component } from "react";
+import "./style.css";
 
-class CardBig extends Component {
-    render() {
-        return (
-            <div className='main-card'>
-                hELLO
-            </div>
-        )
-    }
-}
+import { useDrag } from "react-dnd";
 
+const CardBig = ({ id }) => {
+  const [{ isRowDragging, opacity }, dragRef] = useDrag({
+    type: "CARD",
+    item: {
+      id: id,
+    },
+    collect: (monitor) => ({
+      isRowDragging: monitor.isDragging(),
+      opacity: monitor.isDragging() ? 0.5 : 1,
+    }),
+  });
 
-export default  CardBig
+  return (
+    <div ref={dragRef} id={id} className="main-card" style={{ opacity }}>
+      hELLO {id}
+    </div>
+  );
+};
+
+export default CardBig;
--- src/components/card/cardSmall/index.js
+++ src/components/card/cardSmall/index.js
@@ -1,12 +1,26 @@
-import React, { Component } from 'react'
-import './style.css'
+import React, { Component } from "react";
+import "./style.css";
 
-export default class iindex extends Component {
-    render() {
-        return (
-            <div className='main-card-small'>
-                hELLO small card
-            </div>
-        )
-    }
-}
+import { useDrag } from "react-dnd";
+
+const CardSmall = ({ id }) => {
+  const [{ isRowDragging, opacity }, dragRef] = useDrag({
+    type: "CARD",
+    item: {
+      id: id,
+    },
+    collect: (monitor) => ({
+      isRowDragging: monitor.isDragging(),
+      opacity: monitor.isDragging() ? 0.5 : 1,
+    }),
+  });
+
+  //   console.log("dragging", isRowDragging);
+  return (
+    <div ref={dragRef} id={id} className="main-card-small" style={{ opacity }}>
+      Hello smart card {id}
+    </div>
+  );
+};
+
+export default CardSmall;
